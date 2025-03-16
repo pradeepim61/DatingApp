26282cc6d79e87e24bb18a201a38681a37a48902
Pradeep kumar <pradeepim61@gmail.com>
2025-03-16 17:31:33 -0400

    Added JWT Authentication backend.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,16 +2,15 @@ using System;
 using System.Runtime.ExceptionServices;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")]
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
-
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -20,12 +19,13 @@ public class UsersController(DataContext context) : ControllerBase
         return Ok(users);
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         var user = await context.Users.FindAsync(id);
 
-        if(user == null) return NotFound();
+        if (user == null) return NotFound();
 
         return Ok(user);
     }
