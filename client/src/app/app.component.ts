5eefac77de13593ae987fb10d9791a6b0f7d1e21
Pradeep kumar <pradeepim61@gmail.com>
2025-03-20 20:25:30 -0400

    Client Register and Login.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,27 +2,37 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  private accountService = inject(AccountService);
+
   title = 'DatingApp';
   users: any;
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response ,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
+
   }
-  
+
+  setCurrentUser() {
+    const userstring = localStorage.getItem('user');
+    if (!userstring) return;
+
+    const user = JSON.parse(userstring);
+    this.accountService.currentUser.set(user);
+  }
 
   
+
 }
